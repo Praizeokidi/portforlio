b9465043d898fe1ff988647892851bf0aab59f7d
Praizeokidi <79323596+Praizeokidi@users.noreply.github.com>
2023-03-03 22:41:31 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const toggleMenu = document.querySelector("toggleMenu");
+    const nagivation = document.querySelector(".navigation");
+    toggleMenu.classList.toggle("active");
+    nagivation.classList.toggle("active");
+}
